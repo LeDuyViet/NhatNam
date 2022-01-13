665b160c5accc11e6f5391409f2636fdc0e2f20c
LeDuyViet <71696902+LeDuyViet@users.noreply.github.com>
2022-01-13 16:41:05 +0700

    Update toc.js

--- daututietkiem/toc.js
+++ daututietkiem/toc.js
@@ -1,17 +1,24 @@
-const observer = new IntersectionObserver((entries, observer) => {
-    const toc = document.querySelector('#toc_container')
-    if(!(entries[0].intersectionRatio === 1)) {
-        toc.classList.add('is_scroll')
-        if(!(document.querySelector('.toc_list').style.display))
-        {
-            document.querySelector('.toc_toggle a').click()
-        }
-    } else {
-        toc.classList.remove('is_scroll')
-		document.querySelector('.toc_toggle a').click()
-    }
-}, {
-    threshold: 1
-})
-var h1 = document.querySelector('h1')
-observer.observe(h1)
+jQuery(document).ready(function(){
+		const toc = document.querySelector('#toc_container')
+		if(!document.querySelector('.toc_list').style.display) {
+			document.querySelector('.toc_toggle a').click()
+		}
+		const observer = new IntersectionObserver((entries, observer) => {
+			const toc = document.querySelector('#toc_container')
+			if(!(entries[0].intersectionRatio === 1)) {
+				toc.classList.add('is_scroll')
+				if(!(document.querySelector('.toc_list').style.display))
+				{
+					document.querySelector('.toc_toggle a').click()
+				}
+			} else {
+				toc.classList.remove('is_scroll')
+				document.querySelector('.toc_toggle a').click()
+			}
+		}, {
+			threshold: 1
+		})
+		var h1 = document.querySelector('h1')
+		observer.observe(h1)
+
+	});
